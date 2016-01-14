eb90620ca6fd827debb5fa5c8708f893541a5c22
Tobi <tobistian@gmail.com>
2016-01-14 21:50:28 +0100

    modifyed the test case file

--- spec/features/all_orders_by_customer_test.rb
+++ spec/features/all_orders_by_customer_test.rb
@@ -1,20 +1,22 @@
-require 'rails_helper'
+require 'rails_helper', 'capybara'
 
-describe "customer page", :type => :feature do
+describe "create orders", :type => :feature do
   before :each do
     @customer = create(:customer)
-  end
-
-  it "has a create order link" do
-    visit "/customers/#{@customer.id}"
-    expect(page).to have_link 'New Order'
-  end
-  it "initializes new order correctly" do
     visit "/customers/#{@customer.id}"
     click_link 'New Order'
-    fill_in 'Status', with: :new
     click_button 'Create Order'
-    order = Order.last
-    expect(order.customer).to eq @customer
+    fill_in 'Status', with: :new
+    visit "/customers/#{@customer.id}"
+    click_link 'New Order'
+    click_button 'Create Order'
+    fill_in 'Status', with: :new
+end
+
+  it "shows orders" do
+    visit "/customers/#{@customer.id}/orders"
+    allorders = Order.all
+    
+    expect(page).to have_list 'All Orders'
   end
 end
